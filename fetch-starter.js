7f9f26ff43cd7285199d3512d65c43d1562e8c5d
Metts <metts@Mettss-MacBook-Pro.local>
2019-04-19 09:55:31 -0500

    first commit

---
+++ fetch-starter.js
@@ -0,0 +1,16 @@
+console.log("test")
+
+var startTime = Date.now()
+console.log("Date Begin", startTime);
+for (var i = 0; i <2000000; i++) {
+    var x = i + 1/1 * 6-4;
+}
+console.log("I just looped", i, " times.");
+
+
+// console.log("go get the data");
+// fetch("jeopardy_questions1.json")
+//     .then(result => result.json())
+//     .then(parsed => {
+//         console.log("BIG DATA", parsedResult);
+//     });
